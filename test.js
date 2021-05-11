2f0a1e4724f1e733a13a3814ee5d82409483732a
Eli Doran <eli@elidoran.com>
2021-05-10 20:25:36 -0400

    switch notEqual to not due to depracation

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ tap.test('empty inner arrays', t => {
   const array = [ 0, 1, 2, [[], [[[]]]], 4, 5, 6, 7 ]
 
   const visitor = (_, __, ___, i, ____) => {
-    t.notEqual(i, 3, 'should skip over index 3')
+    t.not(i, 3, 'should skip over index 3')
   }
 
   each(array, visitor)
